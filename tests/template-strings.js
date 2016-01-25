631aeca3689a9a1563cd3cd9b86788ddb5dfedda
Scott Humphries <git@sscotth.io>
2016-01-25 14:59:24 -0600

    more template string tests

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -6,4 +6,16 @@ describe('template strings', function () {
 
     expect(string).to.be.a('string');
   });
+  it('can evaluate variables', function () {
+    var bar = 'baz';
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+  });
+  it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+
+    expect(string).to.equal('The date is: ' + date);
+  });
 });
